abec4f82c1c27dc03bbb8f64cdd063d467556b2b
muithi80 <salomemuithi@gmail.com>
2026-03-28 15:54:25 +0300

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,4 @@
+const reserveBtn = document.getElementById("reserve-btn");
+reserveBtn.addEventListener("click", function(){
+alert("Thank you! We eill contact you soon to confirm your reservation.")
+});
